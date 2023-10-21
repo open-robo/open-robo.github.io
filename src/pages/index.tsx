6391eeefcd5d21cd14712e6cccae0bb98091d3c3
Kris C <kris.kcnotes@gmail.com>
2023-10-21 14:00:22 +1100

    Initial webpage setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={styles.heroBanner}>
+      <video preload="true" autoPlay={true} loop={true} muted={true} className={styles.heroBannerVideo}>
+        <source src="https://fshsrobotics.github.io/file/reel.mp4" type="video/mp4"/>
+      </video>
+      <div className="container">
+        <div className={styles.logoInline}>
+          <img src="/img/logo.svg" width="100px"/>
+          <h1 className={styles.heroBannerTitle}>{siteConfig.title}</h1>
+        </div>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  return (
+    <main>
+      <HomepageHeader />
+    </main>
+  );
+}
